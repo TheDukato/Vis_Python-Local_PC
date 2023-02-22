95ee5b760588dffb2c095b41509f76854f1f99b2
TheDukato <56132686+TheDukato@users.noreply.github.com>
2023-02-22 21:38:15 +0100

    Plot of data from database

--- plotly_ex.py
+++ plotly_ex.py
@@ -7,11 +7,41 @@ import pandas as pd
 
 app = Dash(__name__)
 
+
+import pymysql
+import matplotlib.pyplot as plt
+
+f = open("conf.txt", "r")
+strin = f.readlines()
+
+db = pymysql.connect(host=strin[0][0:-1],user=strin[1][0:-1],password=strin[2][0:-1])
+cursor = db.cursor()
+
+cursor.execute("use ACQ")
+cursor.fetchone()
+
+
+
+cursor.execute("select Value,timeOfProbe from ACQ.historian where ID_Location = 4 AND timeOfProbe > '2023-01-01' AND timeOfProbe < '2023-01-10' ORDER BY timeOfProbe desc ")
+data = cursor.fetchall()
+
+tab = []
+ox = []
+i = 0
+for x in range(len(data)):
+    if data[x][0] < 60:
+        tab.append(data[x][0]*(-0.5)+40)
+        ox.append(data[x][1])
+        i+=1
+
+x = ox
+y = tab
+
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5]#,
+    "Fruit": x,
+    "Amount": y#,
     #"City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
